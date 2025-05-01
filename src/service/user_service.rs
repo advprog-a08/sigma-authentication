b495a49f9ed18dd5102ddc90f7ce96c6a3a7334a
secona <77039267+secona@users.noreply.github.com>
2025-05-01 14:18:23 +0700

    feat: add failing test for user service authenticate

--- src/service/user_service.rs
+++ src/service/user_service.rs
@@ -8,4 +8,47 @@ impl UserService {
     pub fn new(repo: UserRepository) -> Self {
         Self { repo }
     }
+
+    pub fn authenticate(&self, email: String, password: String) -> bool {
+        false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::repository::UserRepository;
+
+    use super::UserService;
+
+    const EMAIL: &str = "asdf@gmail.com";
+    const PASSWORD: &str = "helloworld123";
+
+    #[test]
+    fn test_authenticate_correct() {
+        let email = EMAIL.to_string();
+        let password = PASSWORD.to_string();
+
+        let mut repo = UserRepository::default();
+        repo.create(email.clone(), password.clone());
+
+        let serv = UserService::new(repo);
+        let result = serv.authenticate(email, password);
+
+        assert!(result);
+    }
+
+    #[test]
+    fn test_authenticate_incorrect() {
+        let email = EMAIL.to_string();
+        let password = PASSWORD.to_string();
+        let wrong_password = "asdf".to_string();
+
+        let mut repo = UserRepository::default();
+        repo.create(email.clone(), password.clone());
+
+        let serv = UserService::new(repo);
+        let result = serv.authenticate(email, wrong_password);
+
+        assert!(!result);
+    }
 }
