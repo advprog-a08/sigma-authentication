5561684a4345d57c4300d2791c13b1f552339327
secona <77039267+secona@users.noreply.github.com>
2025-05-01 14:11:44 +0700

    feat: pass test for find user by email

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -3,6 +3,5 @@ use rocket::serde::Serialize;
 #[derive(Debug, Clone, Serialize)]
 #[serde(crate = "rocket::serde")]
 pub struct User {
-    pub id: String,
     pub email: String,
 }
--- src/repository/user_repository.rs
+++ src/repository/user_repository.rs
@@ -14,15 +14,14 @@ impl Default for UserRepository {
 
 impl UserRepository {
     pub fn create(&mut self, email: String) -> User {
-        let id = self.users.len().to_string();
-        let user = User { id: id.clone(), email };
-        self.users.insert(id.clone(), user.clone());
+        let user = User { email: email.clone() };
+        self.users.insert(email, user.clone());
 
         user
     }
 
-    pub fn find_one(&mut self, id: String) -> Option<User> {
-        Some(self.users.get(&id)?.clone())
+    pub fn find_one(&mut self, email: String) -> Option<User> {
+        Some(self.users.get(&email)?.clone())
     }
 }
 
