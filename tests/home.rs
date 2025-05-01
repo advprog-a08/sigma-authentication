a4494baf9c2f768de155c3a99bc5cbebe048733f
secona <77039267+secona@users.noreply.github.com>
2025-05-01 20:31:14 +0700

    feat: reintroduce integration tests for home page

--- tests/home.rs
+++ tests/home.rs
@@ -1,15 +1,17 @@
-// use rocket::local::asynchronous::Client;
-// use rocket::http::Status;
-//
-// use sigma_authentication::app::App;
-// use sigma_authentication::database::setup_test_db;
-//
-// #[rocket::async_test]
-// async fn test_homepage() {
-//     let rocket = App::default().with_pool(setup_test_db().await).rocket();
-//     let client = Client::tracked(rocket).await.expect("valid rocket instance");
-//
-//     let response = client.get("/").dispatch().await;
-//     assert_eq!(response.status(), Status::Ok);
-//     assert_eq!(response.into_string().await.unwrap(), "Hello, world!");
-// }
+mod utils;
+
+use rocket::local::asynchronous::Client;
+use rocket::http::Status;
+
+use sigma_authentication::app::App;
+
+#[rocket::async_test]
+async fn test_homepage() {
+    let test_db = utils::setup_test_db().await;
+    let rocket = App::default().with_pool(test_db.pool).rocket();
+    let client = Client::tracked(rocket).await.expect("valid rocket instance");
+
+    let response = client.get("/").dispatch().await;
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.into_string().await.unwrap(), "Hello, world!");
+}
