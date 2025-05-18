86e7674d6b384ec4d558cdafa3843d0befe2fb77
secona <77039267+secona@users.noreply.github.com>
2025-05-19 00:39:52 +0700

    feat: add failing tests for table session service

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -1,5 +1,7 @@
 mod user_service;
+mod table_session_service;
 mod token_service;
 
 pub use user_service::UserService;
+pub use table_session_service::TableSessionService;
 pub use token_service::TokenService;
