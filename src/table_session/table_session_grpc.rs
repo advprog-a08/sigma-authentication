8949ca661242dc74c47dba9edd29b4d1f66b0dd1
secona <77039267+secona@users.noreply.github.com>
2025-05-21 14:31:23 +0700

    feat: get table session

--- src/table_session/table_session_grpc.rs
+++ src/table_session/table_session_grpc.rs
@@ -32,10 +32,29 @@ impl proto::table_session_service_server::TableSessionService for TableSessionGr
 
         match self.table_session_service.create_session(table_id).await {
             Ok(table_session) => {
-                let session_id = table_session.table_id.to_string();
+                let session_id = table_session.id.to_string();
                 Ok(Response::new(proto::CreateTableSessionResponse { session_id }))
             }
             Err(e) => Err(Status::internal(format!("Failed to create session: {e}")))
+        }
+    }
+
+    async fn get_table_session(
+        &self,
+        request: Request<proto::GetTableSessionRequest>,
+    ) -> Result<Response<proto::GetTableSessionResponse>, Status> {
+        let request = request.into_inner();
+        let session_id = Uuid::from_str(&request.session_id)
+            .map_err(|_| Status::invalid_argument("session_id not a UUID"))?;
+
+        match self.table_session_service.find_by_id(session_id).await {
+            Ok(Some(table_session)) => {
+                Ok(Response::new(proto::GetTableSessionResponse {
+                    table_session: Some(proto::TableSession::from(table_session))
+                }))
+            },
+            Ok(None) => Err(Status::not_found("Table Session not found")),
+            Err(e) => Err(Status::unauthenticated(format!("Unable to get Table Session: {e}"))),
         }
     }
 }
--- src/table_session/table_session_model.rs
+++ src/table_session/table_session_model.rs
@@ -1,6 +1,8 @@
 use chrono::{DateTime, Utc};
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
 use uuid::Uuid;
+
+use super::proto;
 
 #[derive(Debug, Clone, Serialize)]
 pub struct TableSessionModel {
@@ -10,7 +12,12 @@ pub struct TableSessionModel {
     pub created_at: DateTime<Utc>,
 }
 
-#[derive(Debug, Deserialize)]
-pub struct TableSessionCreate {
-    pub table_id: Uuid,
+impl From<TableSessionModel> for proto::TableSession {
+    fn from(value: TableSessionModel) -> Self {
+        Self {
+            id: value.id.to_string(),
+            table_id: value.table_id.to_string(),
+            is_active: value.is_active,
+        }
+    }
 }
