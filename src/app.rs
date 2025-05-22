767615128fd339808d5c3c09f025ebbbc237c306
secona <77039267+secona@users.noreply.github.com>
2025-05-22 11:19:21 +0700

    hotfix: add table session service server

--- src/app.rs
+++ src/app.rs
@@ -5,6 +5,8 @@ use tonic::transport::Server;
 
 use crate::admin::{AdminGrpc, AdminRepository, AdminService};
 use crate::admin::proto::admin_service_server::AdminServiceServer;
+use crate::table_session::{TableSessionGrpc, TableSessionRepository, TableSessionService};
+use crate::table_session::proto::table_session_service_server::TableSessionServiceServer;
 use crate::token::TokenService;
 
 #[derive(Default)]
@@ -25,12 +27,17 @@ impl App {
         let admin_repository = AdminRepository::new(pool.clone());
         let admin_service = AdminService::new(admin_repository);
 
+        let table_session_repository = TableSessionRepository::new(pool.clone());
+        let table_session_service = TableSessionService::new(table_session_repository);
+
         let token_service = TokenService::new("asdf".to_string(), "asdf".to_string());
 
         let admin_grpc = AdminGrpc::new(admin_service, token_service);
+        let table_session_grpc = TableSessionGrpc::new(table_session_service);
 
         Server::builder()
             .add_service(AdminServiceServer::new(admin_grpc))
+            .add_service(TableSessionServiceServer::new(table_session_grpc))
             .serve(addr)
             .await?;
 
